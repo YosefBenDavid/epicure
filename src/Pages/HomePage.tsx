efbdba5b5bac525f735bd5fc11c711b6f6c6fa27
Yosef Ben-David <>
2024-04-21 14:21:20 +0300

    footer display bug fixes

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,17 +4,18 @@ import PopularRestaurants from "../Components/PopularRestaurants/PopularRestaura
 import SignatureDishs from "../Components/SignatureDishs/SignatureDishs.tsx";
 import IconDictionary from "../Components/IconDictionary/IconDictionary.tsx"
 import ChefOfTheWeek from "../Components/ChefOfTheWeek/ChefOfTheWeek.tsx";
+import { Fade } from "react-awesome-reveal";
 
 const HomePage = () => {
 
     return (
-        <>
+        <Fade>
         <HPBanner />
         <PopularRestaurants />
         <SignatureDishs />
         <IconDictionary />
         <ChefOfTheWeek />
-        </>
+        </Fade>
     )
 }
 
